eaf5b447d53b5fa1b6b3b502052936bc91a916e9
Nicolas THIBAUT <nicolas@dev2lead.com>
2016-04-24 21:59:44 +0200

    Update brutexor.py

--- brutexor.py
+++ brutexor.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python
 ##
 # Nicolas THIBAUT
-# http://www.dev2lead.com
+# http://dev2lead.com
 ##
 
 import sys
 
-LEN=int(1024)
-
-class XORFORCE:
-    def __init__(self, file, model):
-        self.file = open(file, "r+")
-        self.model = open(model, "r+")
+class BRUTEXOR:
+    def __init__(self, xorfile, cmpfile):
+        self.xorfile = open(xorfile, "r+b")
+        self.cmpfile = open(cmpfile, "r+b")
     def __del__(self):
-        self.file.close()
-        self.model.close()
-    def execute(self):
-        global LEN
-
-        buffer = self.file.read()
-        list = self.model.read()
-        stack = []
-        for index in range(0, LEN):
-            for key in range(0, 256):
-                if chr(ord(buffer[index]) ^ key) == list[index]:
-                    stack.append(key)
-        for index in range(0, len(buffer)):
-            self.file.seek(index)
-            self.file.write(chr(ord(buffer[index]) ^ stack[index % len(stack)]))
+        self.xorfile.close()
+        self.cmpfile.close()
+    def bruteforce(self, keysize):
+        keydata = []
+        xordata = list(self.xorfile.read())
+        cmpdata = list(self.cmpfile.read())
+        for index in range(0, keysize):
+            for element in range(0, 256):
+                if chr(ord(xordata[index]) ^ ord(chr(element))) == cmpdata[index]:
+                    keydata.append(chr(element))
+        for index in range(0, len(xordata)):
+            xordata[index] = chr(ord(xordata[index]) ^ ord(keydata[index % len(keydata)]))
+        return self.save(xordata)
+    def save(self, data):
+        self.xorfile.seek(0)
+        self.xorfile.write(str().join(data))
+        self.xorfile.flush()
         return 0
 
 def main():
-    global LEN
-
-    if "--len" in sys.argv:
-        LEN = int(sys.argv[sys.argv.index("--len") + 1])
-    if "--file" and "--model" in sys.argv:
-        XORFORCE(sys.argv[sys.argv.index("--file") + 1], sys.argv[sys.argv.index("--model") + 1]).execute()
+    keysize = 256
+    if "--keysize" in sys.argv:
+        keysize = sys.argv[sys.argv.index("--keysize") + 1]
+    if "--xorfile" in sys.argv and "--cmpfile" in sys.argv:
+        xorfile = sys.argv[sys.argv.index("--xorfile") + 1]
+        cmpfile = sys.argv[sys.argv.index("--cmpfile") + 1]
+        BRUTEXOR(xorfile, cmpfile).bruteforce(keysize)
     return 0
 
 if __name__ == "__main__":
